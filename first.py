de84329bdafae260f31aaa46261d75d7c16689bb
ALABYEKKUBO SSUUBI BRIAN <106748613+BRIAN12682@users.noreply.github.com>
2024-06-18 11:14:47 +0300

    Update first.py

--- first.py
+++ first.py
@@ -1,2 +1,5 @@
-x = input("Please enter you name")
-print(f"Welcome to this github repository, Mr/Mrs {x}")
+def welcome_message(x):
+  x = input("Please enter you name")
+  print(f"Welcome to this github repository, Mr/Mrs {x}")
+  return (f"Welcome to this github repository, Mr/Mrs {x}")
+welcome_message(x)
